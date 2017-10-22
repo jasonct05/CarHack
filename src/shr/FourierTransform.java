6982f1b079782a9005a170002086f463b438aa87
Clifford Silfanus <Cliff@D-69-91-165-245.dhcp4.washington.edu>
2017-10-21 22:40:10 -0700

    comment code

--- src/shr/FourierTransform.java
+++ src/shr/FourierTransform.java
@@ -3,7 +3,7 @@ package shr;
 public class FourierTransform {
 
     public static final int Fs = 20000;
-    public static final double T = 1/Fs;
+    public static final double T = 1 / Fs;
     public static final int L = 6000;
     public static final int n = 15;
     public static final int INPUT_LENGTH = 32768;
@@ -20,6 +20,19 @@ public class FourierTransform {
         double[] frequency = getFrequency(fftArray);
     }
 
+    /*
+    public static final int MAX_FREQ = 2000;
+    public static final int RANGE = 20;
+    public static final int INDEX = amplitude.length;
+
+    double[] plotTable = new double[MAX_FREQ / RANGE];
+
+    for(int i = 0; i < INDEX; i++) {
+        if (frequency[i] <= MAX_FREQ) {
+            plotTable[(frequency[i] - 1) / RANGE] = amplitude[i];
+        }
+    }
+    */
 
     private static Complex[] createComplexArray(int[] input) {
         Complex[] complexArr = new Complex[INPUT_LENGTH];
